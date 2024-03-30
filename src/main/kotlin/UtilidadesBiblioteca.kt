86459101349cf26e719df2edb6cfb1e7230f4152
aaracan246 <acalberto@iesrafaelalberti.es>
2024-03-30 18:30:48 +0100

    ejercicio-2-metodos-estaticos (documentado)

--- src/main/kotlin/UtilidadesBiblioteca.kt
+++ src/main/kotlin/UtilidadesBiblioteca.kt
@@ -3,9 +3,12 @@ package org.example
 class UtilidadesBiblioteca {
 
     companion object{
-
         val setDeId = mutableSetOf<Int>()
 
+        /** Función encargada de generar un identificador único.
+         *
+         * @return id de tipo Int.
+         * */
         fun generarIdentificadorUnico(): Int{
             val id = (1..2000).random()
             setDeId.add(id)
